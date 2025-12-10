fad42fc75d3554d7b9c6cccda3b4d658f51aede8
Nikolay Movchanenko <movchanenko.nn@gmail.com>
2025-12-10 13:25:17 +0100

    Revert "feat: set 'catpuccin' theme"
    This reverts commit 0f4bcb286af38c92e958163d36c7fc37bc293538.

--- init.lua
+++ init.lua
@@ -120,7 +120,6 @@ end)
 
 -- Enable break indent
 vim.o.breakindent = true
--- the only opts helped to fix ugly indentation in JS/TS
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
@@ -886,20 +885,20 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      -- require('catppuccin').setup {
-      --   styles = {
-      --     comments = { italic = false }, -- Disable italics in comments
-      --   },
-      -- }
-      --
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
